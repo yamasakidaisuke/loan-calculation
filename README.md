f2cb2c9389260961b5735ef47f9ca47480b9fc2b
DESKTOP-1825JMU\lifes <lifesteps40@gmail.com>
2022-01-07 12:02:17 +0900

    docs:ボーナス払い

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # loan-calculation
 ローン試算
+
+# 仕様
+元利金等返済
+
+## 参考サイト
+単位を不動産ナビ様に合わせる
+[https://www.fudosan-navi.com/juutakuloan-excel-calculation/]
+
+コード参考
+[https://note.com/jisakuhp/n/n129ed073d3e4]
+[https://kamatalog.com/calc-form/]
+
+計算式参考
+[https://mponline.sbi-moneyplaza.co.jp/housingloan/articles/20200903risokukeisan.html]
+
+元利均等返済と元金均等返済の違いについて
+[https://www.jibunbank.co.jp/column/article/00282/]
--- js/functions.js
+++ js/functions.js
@@ -9,12 +9,12 @@ function PMT() {
 	var bonus = parseFloat(document.loan.bonus.value);//ボーナス予定返済額（1回分）
 	var bonusN = parseFloat(document.loan.bonusN.value);//ボーナス予定返済回数（n）
 	var ir = parseFloat(document.loan.ir.value);//金利
-	var np = parseFloat(document.loan.np.value);//返済期間（月数）
+	var np = parseFloat(document.loan.np.value);//返済期間（年数）
 //「floor関数」は小数点以下の値を切り捨てた結果を返します。
 //「pow関数」は1番目の引数を、2番目の引数で累乗した値を返します。
 // Math.pow() 関数は base を exponent 乗した値、つまり、baseexponent の値を返します。
 //「toLocaleString関数」で整数部分を3桁毎に区切り文字を挿入した形式で文字列に変換した結果を返します。（ロケールに応じたNumberクラスのオブジェクトの文字列表現を返します。）
-return Math.floor((((pv - (bonus * bonusN * (np / 12))) - deposit)*10000 * (ir / 100 / 12) * (Math.pow(1 + (ir / 100 / 12), np))) / (Math.pow(1 + (ir / 100 / 12), np) -1)).toLocaleString();
+return Math.floor((((pv - (bonus * bonusN * np)) - deposit) * (ir / 100 /12) * (Math.pow(1 + (ir / 100 /12), np*12))) / (Math.pow(1 + (ir / 100 / 12), np*12) -1)).toLocaleString();
 }
 
 function PMT2() {
@@ -23,13 +23,12 @@ function PMT2() {
 	var deposit = parseFloat(document.loan.deposit.value);//頭金
 	var bonus = parseFloat(document.loan.bonus.value);//ボーナス予定返済額（1回分）
 	var bonusN = parseFloat(document.loan.bonusN.value);//ボーナス予定返済回数（n）
-	var ir = parseFloat(document.loan.ir.value);//金利
+	var ir = parseFloat(document.loan.ir.value);//金利（年）
 	var np = parseFloat(document.loan.np.value);//返済期間（月数）
 //「floor関数」は小数点以下の値を切り捨てた結果を返します。
 //「pow関数」は1番目の引数を、2番目の引数で累乗した値を返します。
 // Math.pow() 関数は base を exponent 乗した値、つまり、baseexponent の値を返します。
 //「toLocaleString関数」で整数部分を3桁毎に区切り文字を挿入した形式で文字列に変換した結果を返します。（ロケールに応じたNumberクラスのオブジェクトの文字列表現を返します。）
 //return Math.floor((ir /bonusN)/(Math.pow(1+ir/2,np*2)-1)* ((bonus * bonusN * (np / 12)) *10000)*Math.pow(1 + ir/bonusN,np*bonusN)+ 0 ).toLocaleString();
-return Math.floor((((pv - (bonus * bonusN * (np / 12))) - deposit)*10000 * (ir / 100 / 12) * (Math.pow(1 + (ir / 100 / 12), np))) / (Math.pow(1 + (ir / 100 / 12), np) -1)).toLocaleString();
-
+return Math.floor(((bonus * bonusN * np) *(ir/100/bonusN)*(Math.pow ( 1 +(ir/100/bonusN),(np*bonusN)))) / ((Math.pow(1+(ir/100/bonusN),(np*bonusN)))-1)).toLocaleString();
 }
